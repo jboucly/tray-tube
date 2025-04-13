596a791503322313464077f2e7b6bdc611428cd3
Jboucly <julien_boucly@ext.carrefour.com>
2025-04-13 16:28:52 +0200

    feat: integrate vue-router and create home view for navigation

---
+++ src/ui/router/index.ts
@@ -0,0 +1,15 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView
+    }
+];
+
+export const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+});
---
+++ src/ui/views/homeView.vue
@@ -0,0 +1,6 @@
+<template>
+    <title>TrayTube</title>
+    <div>
+        <h1>Bienvenue sur TrayTube</h1>
+    </div>
+</template>
