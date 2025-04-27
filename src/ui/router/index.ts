dcef3726c06bf4f7eebd451a66f1fd3fb9b9f8fc
Jboucly <julien_boucly@ext.carrefour.com>
2025-04-27 17:22:33 +0200

    fix: correct casing in import statement for HomeView component

--- src/ui/router/index.ts
+++ src/ui/router/index.ts
@@ -1,6 +1,6 @@
 import SettingsView from '@/views/SettingsView.vue';
 import { createRouter, createWebHashHistory } from 'vue-router';
-import HomeView from '../views/homeView.vue';
+import HomeView from '../views/HomeView.vue';
 
 const routes = [
     {
